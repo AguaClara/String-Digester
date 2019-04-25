8bdcff1cc1ce2bfec8db3fbf2bdcf55ef325ba97
Gabriela Sibel <gvs27@cornell.edu>
2019-04-25 19:48:13 -0400

    oops forgot the code sorry for pushing so many things

--- Spring 2019/Python_Code_Dacron.py
+++ Spring 2019/Python_Code_Dacron.py
@@ -2,31 +2,31 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 ###############################################################################
-FR,VWC = np.loadtxt('CFRT_FR_vs_VWC.csv',
+FR,VWS = np.loadtxt('DFRT_FR_vs_VWS.csv',
                 unpack = True,
                 delimiter = ',')
 #FR = flow rate
 #VWC = volume of water on the chain
-length = .9144 # length of the chain(can be changed)
+length = .78 # length of the chain(can be changed)
 optimalrestime = 900 # optimal residence time in seconds(can be changed)
 ##############################################################################
 plt.figure(0)
-plt.plot(FR,VWC, 'ro')
-plt.title('Volume of Water on Chain at \nDifferent Flow Rates')
+plt.plot(FR,VWS, 'ro')
+plt.title('Volume of Water on Dacron String at \nDifferent Flow Rates')
 plt.xlabel('Flow Rate (mL/s)')
-plt.ylabel('Volume of Water on Chain (mL)')
-plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/CFRvsVWC')
+plt.ylabel('Volume of Water on Dacron String (mL)')
+plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/DFRvsVWC')
 ##############################################################################
 RT = [0]*len(FR)
 for i in range(len(FR)):
-    RT[i] = VWC[i]/FR[i] #RT residence time
+    RT[i] = VWS[i]/FR[i] #RT residence time
 ##############################################################################
 plt.figure(1)
 plt.plot(FR,RT, 'ro')
-plt.title('Residence Time of Water on Chain \nat Different Flow Rates')
+plt.title('Residence Time of Water on Dacron String \nat Different Flow Rates')
 plt.xlabel('Flow Rate (mL/s)')
 plt.ylabel('Residence Time (s)')
-plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/CFRvsRT')
+plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/DFRvsRT')
 ##############################################################################
 secperm = [0]*len(RT)
 HN = [0]*len(RT)
@@ -36,7 +36,7 @@ for i in range(len(RT)):
 ###############################################################################
 plt.figure(2)
 plt.plot(FR,HN, 'ro')
-plt.title('Height of Chain Needed for a \n15 Minute Residence Time')
+plt.title('Height of Dacron String Needed for a \n15 Minute Residence Time')
 plt.xlabel('Flow Rate (mL/s)')
 plt.ylabel('Height of Chain (m)')
-plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/CFRvsHN')
+plt.savefig('/Users/gvsso/OneDrive/Documents/Gaby/String-Digester/Spring 2019/images/DFRvsHN')
